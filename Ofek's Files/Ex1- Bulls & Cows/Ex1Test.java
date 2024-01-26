bab9cfb0a3fdf89cdd8c0dd57c1ee5ca87c86771
ofekbarshalom <155369472+ofekbarshalom@users.noreply.github.com>
2024-01-26 13:03:40 +0200

    Update Ex1Test.java

--- Ofek's Files/Ex1- Bulls & Cows/Ex1Test.java
+++ Ofek's Files/Ex1- Bulls & Cows/Ex1Test.java
@@ -1,36 +1,38 @@
-import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 public class Ex1Test {
+    public static double avg2=0,avg3=0,avg4=0,avg5=0,avg6=0;
     @Test
     public void avg2(){
-        double avg2=avgCheck(2);
-        System.out.println("the average in 2 digits is:" +avg2);
-        assertTrue(avg2>0);
+        avg2=avgCheck(2);
+        assertTrue(avg2<8);
     }
     @Test
     public void avg3(){
-        double avg3=avgCheck(3);
-        System.out.println("the average in 3 digits is:" +avg3);
-        assertTrue(avg3>0);
+        avg3=avgCheck(3);
+        assertTrue(avg3<9);
     }
     @Test
     public void avg4(){
-        double avg4=avgCheck(4);
-        System.out.println("the average in 4 digits is:" +avg4);
-        assertTrue(avg4>0);
+        avg4=avgCheck(4);
+        assertTrue(avg4<10);
     }
     @Test
     public void avg5(){
-        double avg5=avgCheck(5);
-        System.out.println("the average in 5 digits is:" +avg5);
-        assertTrue(avg5>0);
+        avg5=avgCheck(5);
+        assertTrue(avg5<10);
     }
     @Test
     public void avg6(){
-        double avg6=avgCheck(6);
-        System.out.println("the average in 6 digits is:" +avg6);
-        assertTrue(avg6>0);
+        avg6=avgCheck(6);
+        assertTrue(avg6<10);
+        PrintAverage();
     }
+
+    /** avgCheck:
+     * Checks the average of the rounds of 100 games.
+     * @param numDigits The number of digits in the code
+     * @return the average of the rounds.
+     */
     public double avgCheck(int numDigits){
         Ex1.numOfDigits=numDigits;             // global int that represent the number of digits in the code
         for(int i=0;i<100;i++){                //loop over the main in Ex1 100 times to get an average rounds number.
@@ -39,5 +41,16 @@ public class Ex1Test {
         double avg=(double) Ex1.counter/100;   //Divide the counter to get the average of 100 runs
         Ex1.counter=0;                         //Reset the counter so we could use it again later
         return avg;
+    }
+
+    /**PrintAverage:
+     * Prints to guesses average for each digit and for all together
+     */
+    public static void PrintAverage() {
+        System.out.println("the averages are:");
+        System.out.println("2) " + avg2 + " 3) " + avg3 + " 4) " + avg4 + " 5) " + avg5 + " 6) " + avg6);
+        double allAvgs=(avg2+avg3+avg4+avg5+avg6)/5;
+        allAvgs=Math.round(allAvgs*100.0)/100.0;           //round the avarge to 2 digits after the dot
+        System.out.println("Average all: "+allAvgs);
     }
 }
