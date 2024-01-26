7ebe0cd9ff137251abe1bf9c25c13ac92d8e5cb2
RoyNaor <133534625+RoyNaor@users.noreply.github.com>
2024-01-26 15:25:56 +0200

    Update Ex1Test.java

--- Roy's Files/Bulls&Cows/Ex1Test.java
+++ Roy's Files/Bulls&Cows/Ex1Test.java
@@ -2,36 +2,47 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class Ex1Test {
-
+    
     @Test
     public void test2Digits() {
         double avg = testAverageRounds(2);
         System.out.println("Average for 2 digits: " + avg);
+        assertTrue(avg < 9.5);
     }
 
     @Test
     public void test3Digits() {
         double avg = testAverageRounds(3);
         System.out.println("Average for 3 digits: " + avg);
+        assertTrue(avg < 9.5);
     }
 
     @Test
     public void test4Digits() {
         double avg = testAverageRounds(4);
         System.out.println("Average for 4 digits: " + avg);
+        assertTrue(avg < 9.5);
     }
 
     @Test
     public void test5Digits() {
         double avg = testAverageRounds(5);
         System.out.println("Average for 5 digits: " + avg);
+        assertTrue(avg < 9.5);
     }
 
     @Test
     public void test6Digits() {
         double avg = testAverageRounds(6);
         System.out.println("Average for 6 digits: " + avg);
+        assertTrue(avg < 9.5);
     }
+
+    /** -testAverageRounds-
+     * Calculates the average number of rounds required by the Ex1.main method over 100 runs
+     * @param numOfDigits The number of digits to be used in the Ex1.main method for the secret code
+     * @return The average number of rounds taken by the Ex1.main method over 100 runs
+     */
     public double testAverageRounds(int numOfDigits) {
         Ex1.numOfDigits = numOfDigits;
         int totalRounds = 0; // sum the total of all rounds in 100 runs
