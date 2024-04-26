c51df3938e8cd1fe064a8a513b3d287e50eae7c3
amitd55 <156925877+amitd55@users.noreply.github.com>
2024-04-26 19:47:05 +0300

    Q_2.java

--- Amit_codes/src/ExampleExam/Q_2.java
+++ Amit_codes/src/ExampleExam/Q_2.java
@@ -6,17 +6,13 @@ import java.util.Collections;
 public class Q_2 {
     public static void main(String[]args)
     {
-        String s1="yaBen";
-        String s2="Sharmut";
-        String s3="Amen";
-        String s4="Tamut";
-        String s5="Tamut yaBen Sharmut";
+        String s1="yalla";
+        String s2="bye";
+        String s3="bye yalla";
         ArrayList<String>words=new ArrayList<>();
         words.add(s1);
         words.add(s2);
-        words.add(s3);
-        words.add(s4);
-        System.out.println(isPer(words,s5));
+        System.out.println(isPer(words,s3));
 
 
 
